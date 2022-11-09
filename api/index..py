33a54f89ad35a2826ffb41458ccb4d61c5de3df3
Agee <111475184+dyagee@users.noreply.github.com>
2022-11-09 23:49:26 +0100

    Update index..py

--- api/index..py
+++ api/index..py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #def catch_all(path):
 #    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 #   return Response(put_text('Awesome modifications!'), mimetype="text")
-@app.route('/')
+@app.route('/web')
 def run():
     put_text('Hello Flaskies!')
 

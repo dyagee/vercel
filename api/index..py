22d8cd863b9b497052aab3880546bc1b36343c73
Agee <111475184+dyagee@users.noreply.github.com>
2022-11-09 23:44:24 +0100

    Update index..py

--- api/index..py
+++ api/index..py
@@ -2,9 +2,14 @@ from flask import Flask, Response
 from pywebio.output import *
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-    return Response(put_text('Awesome modifications!'), mimetype="text")
+#@app.route('/', defaults={'path': ''})
+#@app.route('/<path:path>')
+#def catch_all(path):
+#    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+#   return Response(put_text('Awesome modifications!'), mimetype="text")
+@app.route('/')
+put_text('Hello Flaskies!')
+
+if __name__ == '__main__':
+    app.run()
 

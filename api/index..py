32fcd8ac9a72e96e407b25b9a0b5988363ac5c3e
dyagee <ageeaondo45@gmail.com>
2022-11-09 23:12:12 +0100

    added pywebio

--- api/index..py
+++ api/index..py
@@ -1,7 +1,10 @@
 from flask import Flask, Response
+from pywebio.output import *
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+put_text("Hello flask Pywebio combo")
